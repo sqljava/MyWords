7019cbb446636223c66aa3004be4b7450c0b655b
sqljava <sqljava456@gmail.com>
2024-04-08 13:18:42 +0500

    home view

--- app/src/main/java/uz/ictschool/mywords/ui/theme/Color.kt
+++ app/src/main/java/uz/ictschool/mywords/ui/theme/Color.kt
@@ -8,4 +8,7 @@ val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFF5DB963)
+
+val myGreen = Color(0xFF4CAF50)
+val myGree = Color(0xFF4CAF50)
